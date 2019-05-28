74cc38f24d79a658e12714e643da4a4c99fc01b0
David Orlea <david.orlea@gmail.com>
2019-05-28 17:29:33 +0200

    Temporarily disable restarting and reloading of Home Assistant in deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,33 +20,33 @@ case "$1" in
         echo -n "Deploying everything... "
         rsync --checksum --delete --human-readable --recursive --exclude-from=deploy/exclude.conf . "$HASS_SSH_URL"
         echo "done."
-        echo -n "Restarting Home Assistant... "
-        curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/homeassistant/restart
-        echo "done."
+        # echo -n "Restarting Home Assistant... "
+        # curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/homeassistant/restart
+        # echo "done."
         ;;
     "automations" )
         echo -n "Deploying automations... "
         rsync --checksum --delete --human-readable --recursive --exclude-from=deploy/exclude.conf automations/ "$HASS_SSH_URL"/automations
         echo "done."
-        echo -n "Reloading automations... "
-        curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/automation/reload
-        echo "done."
+        # echo -n "Reloading automations... "
+        # curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/automation/reload
+        # echo "done."
         ;;
     "groups" )
         echo -n "Deploying groups... "
         rsync --checksum --delete --human-readable --recursive --exclude-from=deploy/exclude.conf groups.yaml "$HASS_SSH_URL"
         echo "done."
-        echo -n "Reloading groups... "
-        curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/group/reload
-        echo "done."
+        # echo -n "Reloading groups... "
+        # curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/group/reload
+        # echo "done."
         ;;
     "scripts" )
         echo -n "Deploying scripts... "
         rsync --checksum --delete --human-readable --recursive --exclude-from=deploy/exclude.conf scripts/ "$HASS_SSH_URL"/scripts
         echo "done."
-        echo -n "Reloading scripts... "
-        curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/script/reload
-        echo "done."
+        # echo -n "Reloading scripts... "
+        # curl -s -S -o /dev/null -X POST -H "Content-Type: application/json" "$HASS_WEB_URL"/api/services/script/reload
+        # echo "done."
         ;;
     *)
         echo "Usage: $0 <all | automations | groups | scripts>"
