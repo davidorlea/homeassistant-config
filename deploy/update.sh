a7e3953584cc05123c3e6ba38a3d2641472dfde6
David Orlea <david.orlea@gmail.com>
2020-12-06 14:37:18 +0100

    Add deployment script to update Home Assistant from remote

---
+++ deploy/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$HASS_SSH_URL" ]; then
+    echo "You need to set deployment parameter HASS_SSH_URL"
+    exit 1
+fi
+
+ssh "$HASS_SSH_URL" << 'EOF'
+    source $VENV_DIR/bin/activate
+    pip install -U setuptools wheel
+    pip install -r requirements.txt
+    deactivate
+EOF
