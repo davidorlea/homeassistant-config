f1c46630c6a9d850c312034c1a615a57fc8d164e
David Orlea <david.orlea@gmail.com>
2021-10-16 12:40:36 +0200

    Add logging of Python and PIP version to update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,8 @@ fi
 
 ssh "$HASS_SSH_URL" << 'EOF'
     source $VENV_DIR/bin/activate
+    python --version
+    pip --version
     pip install -U setuptools wheel
     pip install -r requirements.txt
     deactivate
