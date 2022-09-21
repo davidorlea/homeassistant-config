72bad5358ceb778795c507634ff79f17ed22394f
David Orlea <david.orlea@gmail.com>
2022-09-21 17:35:44 +0200

    Update custom component powercalc to 0.26.0

--- custom_components/powercalc/config_flow.py
+++ custom_components/powercalc/config_flow.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import copy
 import logging
-from audioop import mul
 from typing import Any
 
 import voluptuous as vol
@@ -22,7 +21,7 @@ from homeassistant.const import (
     Platform,
 )
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import FlowHandler, FlowResult
+from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import selector
 
 from .common import SourceEntity, create_source_entity
@@ -49,7 +48,6 @@ from .const import (
     CONF_POWER_TEMPLATE,
     CONF_SENSOR_TYPE,
     CONF_STANDBY_POWER,
-    CONF_START_TIME,
     CONF_STATES_POWER,
     CONF_SUB_GROUPS,
     CONF_SUB_PROFILE,
@@ -175,7 +173,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     def __init__(self):
         """Initialize options flow."""
-        self.sensor_config: dict[str, Any] = dict()
+        self.sensor_config: dict[str, Any] = {}
         self.selected_sensor_type: str | None = None
         self.name: str | None = None
         self.source_entity: SourceEntity | None = None
@@ -376,7 +374,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     self.sensor_config.get(CONF_MODEL),
                 )
             )
-            sub_profiles = await library.get_subprofile_listing(profile)
+            sub_profiles = profile.get_sub_profiles()
             if sub_profiles:
                 return await self.async_step_lut_subprofile()
             errors = await self.validate_strategy_config()
@@ -714,7 +712,7 @@ async def _create_lut_schema_subprofile(
     profile = await library.get_profile(model_info)
     sub_profiles = [
         selector.SelectOptionDict(value=sub_profile, label=sub_profile)
-        for sub_profile in await library.get_subprofile_listing(profile)
+        for sub_profile in profile.get_sub_profiles()
     ]
     return vol.Schema(
         {
