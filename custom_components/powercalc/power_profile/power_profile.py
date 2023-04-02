1b9f5db0ee02b7a3107da2e7bfa527a96a2257ce
David Orlea <david.orlea@gmail.com>
2023-04-02 15:17:55 +0200

    Update custom component powercalc to 1.4.0

--- custom_components/powercalc/power_profile/power_profile.py
+++ custom_components/powercalc/power_profile/power_profile.py
@@ -188,7 +188,7 @@ class PowerProfile:
         return self._json_data.get("config_flow_discovery_remarks")
 
     def get_sub_profiles(self) -> list[str]:
-        """Get listing op possible sub profiles"""
+        """Get listing of possible sub profiles"""
         return sorted(next(os.walk(self.get_model_directory(True)))[1])
 
     @property
@@ -234,7 +234,7 @@ class PowerProfile:
         if (
             self.device_type == DeviceType.SMART_SWITCH
             and entity_entry
-            and entity_entry.platform in ["switch_as_x", "hue"]
+            and entity_entry.platform in ["hue"]
         ):
             return True
         return DEVICE_DOMAINS[self.device_type] == source_entity.domain
@@ -294,6 +294,8 @@ class SubProfileSelector:
                 matcher_config["entity_id"],
                 matcher_config["map"],
             )
+        if matcher_type == "entity_id":
+            return EntityIdMatcher(matcher_config["pattern"], matcher_config["profile"])
         raise PowercalcSetupError(f"Unknown sub profile matcher type: {matcher_type}")
 
 
@@ -350,6 +352,21 @@ class AttributeMatcher(SubProfileMatcher):
             return None
 
         return self._mapping.get(val)
+
+    def get_tracking_entities(self) -> list[str]:
+        return []
+
+
+class EntityIdMatcher(SubProfileMatcher):
+    def __init__(self, pattern: str, profile: str):
+        self._pattern = pattern
+        self._profile = profile
+
+    def match(self, entity_state: State) -> str | None:
+        if re.search(self._pattern, entity_state.entity_id):
+            return self._profile
+
+        return None
 
     def get_tracking_entities(self) -> list[str]:
         return []
