9e9412124a05674a604840f2430a6f9744643894
David Orlea <david.orlea@gmail.com>
2026-04-18 11:07:05 +0200

    Update custom component aseag next bus to 0.9.0

--- custom_components/aseag_next_bus/sensor.py
+++ custom_components/aseag_next_bus/sensor.py
@@ -1,15 +1,19 @@
 """Representation of ASEAG Next Bus Sensors."""
 
+from datetime import datetime
 import logging
-
-import requests
-import voluptuous as vol
+from typing import Any, cast
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorDeviceClass
 from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.util.dt import utc_from_timestamp, utcnow
+import requests
+import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -43,13 +47,18 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def setup_platform(_1, config, add_entities, _2):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the sensor platform."""
 
-    stop_id = config.get(CONF_STOP_ID)
-    track = config.get(CONF_TRACK)
-    mode = config.get(CONF_MODE)
-    name = config.get(CONF_NAME)
+    stop_id: str = config[CONF_STOP_ID]
+    track: str = config[CONF_TRACK]
+    mode: str = config[CONF_MODE]
+    name: str = config[CONF_NAME]
 
     api = AseagApi()
     add_entities([AseagNextBusSensor(api, name, mode, stop_id, track)])
@@ -59,88 +68,103 @@ class AseagApi:
     """Representation of the ASEAG API."""
 
     @staticmethod
-    def get_predictions(stop_id):
+    def get_predictions(stop_id: str) -> dict[str, Any] | None:
         """Get predictions matching a stop from the ASEAG API."""
         headers = {"User-Agent": "curl/7.64.1"}
         resource = f"https://mova.aseag.de/mbroker/rest/areainformation/{stop_id}"
         try:
-            response = requests.get(resource, headers=headers, verify=True, timeout=10)
+            response = requests.get(
+                resource, headers=headers, verify=True, timeout=(5, 10)
+            )
             response.raise_for_status()
-            return response.json()
+            return cast(dict[str, Any], response.json())
+        except requests.exceptions.JSONDecodeError as ex:
+            _LOGGER.error("Error parsing data: %s failed with %s", resource, ex)
+            return None
+        except requests.exceptions.HTTPError as ex:
+            if ex.response.status_code >= 500:
+                _LOGGER.debug("Error fetching data: %s failed with %s", resource, ex)
+            else:
+                _LOGGER.error("Error fetching data: %s failed with %s", resource, ex)
+            return None
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as ex:
+            _LOGGER.debug("Error fetching data: %s failed with %s", resource, ex)
+            return None
         except requests.exceptions.RequestException as ex:
             _LOGGER.error("Error fetching data: %s failed with %s", resource, ex)
-            return None
-        except ValueError as ex:
-            _LOGGER.error("Error parsing data: %s failed with %s", resource, ex)
             return None
 
 
 class AseagNextBusSensor(Entity):
     """Representation of a ASEAG Next Bus Sensor."""
 
-    def __init__(self, api, name, mode, stop_id, track):
+    def __init__(
+        self, api: AseagApi, name: str, mode: str, stop_id: str, track: str
+    ) -> None:
         """Initialize the ASEAG Next Bus Sensor."""
-        self._api = api
-        self._name = name
-        self._mode = mode
-        self._stop_id = stop_id
-        self._track = track
-        self._predictions = []
-        self._state = None
-        self._attributes = {}
+        self._api: AseagApi = api
+        self._name: str = name
+        self._mode: str = mode
+        self._stop_id: str = stop_id
+        self._track: str = track
+        self._predictions: list[dict[str, Any]] = []
+        self._state: str | int | None = None
+        self._attributes: dict[str, Any] = {}
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the ASEAG Next Bus Sensor."""
         return f"{self._name} {self._stop_id} {self._track}"
 
     @property
-    def device_class(self):
+    def device_class(self) -> SensorDeviceClass | None:
         """Return the device class of the ASEAG Next Bus Sensor."""
         if self._mode == "single":
             return SensorDeviceClass.TIMESTAMP
+        return None
 
     @property
-    def icon(self):
+    def icon(self) -> str:
         """Icon to use in the frontend of the ASEAG Next Bus Sensor."""
         return ICON
 
     @property
-    def state(self):
+    def state(self) -> str | int | None:
         """Return the state of the ASEAG Next Bus Sensor."""
         return self._state
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, Any]:
         """Return the state attributes of the ASEAG Next Bus Sensor."""
         return self._attributes
 
-    def update(self):
+    def update(self) -> None:
         """Fetch new state data for the ASEAG Next Bus Sensor."""
         self._state = None
         self._attributes = {}
 
-        now = utcnow()
-        result = self._api.get_predictions(self._stop_id)
-        predictions = []
+        now: datetime = utcnow()
+        result: dict[str, Any] | None = self._api.get_predictions(self._stop_id)
+        predictions: list[Any] = []
 
         if result:
             try:
                 predictions = [
                     d["stopPrediction"] for d in result["departures"]["departures"]
                 ]
-            except KeyError as ex:
+            except (KeyError, TypeError) as ex:
                 _LOGGER.error(
-                    "Erroneous result found when expecting list of predictions: %s", ex
+                    "Erroneous result found: %s failed with %s",
+                    result,
+                    ex,
                 )
-        else:
-            _LOGGER.error("Empty result found when expecting list of predictions")
 
         for p in self._predictions:
             if not any(p["tripId"] in subl.values() for subl in predictions):
                 predictions.append(p)
 
         predictions = [p for p in predictions if p["track"] == self._track]
+        predictions = [p for p in predictions if p["cancelled"] is False]
         predictions = [p for p in predictions if self.__get_prediction_time(p) >= now]
 
         self._predictions = sorted(
@@ -170,15 +194,15 @@ class AseagNextBusSensor(Entity):
             self._attributes[ATTR_ATTRIBUTION] = ATTRIBUTION
 
     @staticmethod
-    def __get_prediction_time(prediction):
+    def __get_prediction_time(prediction: dict[str, Any]) -> datetime:
         """Return prediction time derived from planned and actual time."""
-        if prediction["plannedTime"]:
-            return utc_from_timestamp(
-                int((prediction["actualTime"] or prediction["plannedTime"]) / 1000)
-            )
+        if "actualTime" in prediction:
+            return utc_from_timestamp(int(prediction["actualTime"] / 1000))
+        return utc_from_timestamp(int(prediction["plannedTime"] / 1000))
 
     @staticmethod
-    def __get_prediction_delay(prediction):
+    def __get_prediction_delay(prediction: dict[str, Any]) -> int | None:
         """Return prediction delay derived from planned and actual time."""
-        if prediction["actualTime"] and prediction["plannedTime"]:
+        if "actualTime" in prediction and "plannedTime" in prediction:
             return int((prediction["actualTime"] - prediction["plannedTime"]) / 1000)
+        return None
